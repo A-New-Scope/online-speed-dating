5f8f952eef3b8120d497623ad52ddc7a01eb67a6
Nim G <nimrodgrinvald+github@gmail.com>
2016-12-15 19:12:37 -0800

    Change FB auth routing paths to account for passing through /router to get to /authRouter

--- App/Server/routes/authRouter.js
+++ App/Server/routes/authRouter.js
@@ -10,13 +10,13 @@ router.post('/login', passport.authenticate('local'), function(req, res) {
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
 //     /auth/facebook/callback
-router.get('/auth/facebook', passport.authenticate('facebook'));
+router.get('/facebook', passport.authenticate('facebook'));
 
 // Facebook will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
 // access was granted, the user will be logged in.  Otherwise,
 // authentication has failed.
-router.get('/auth/facebook/callback',
+router.get('/facebook/callback',
   passport.authenticate('facebook', {
     successRedirect: '/',
     failureRedirect: '/login'
