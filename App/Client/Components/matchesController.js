da000a1f0e4be2a6ff4531d89e92864278504337
Isaac <Isaac@Isaacs-MacBook-Pro.local>
2016-12-17 15:47:08 -0800

    async get matches

--- App/Client/Components/matchesController.js
+++ App/Client/Components/matchesController.js
@@ -13,19 +13,19 @@ var matches = {
       })
     },
 
-    likeCallee: function($http) {
-      var caller = 'test';//this.$store.state.user.username
-      var callee = 'doggie';
-      this.$http.post('/api/likes', {
-        sessionUser: caller,
-        likedUser: callee
-      });
-    }
+    // likeCallee: function($http) {
+    //   var caller = 'test';//this.$store.state.user.username
+    //   var callee = 'doggie';
+    //   this.$http.post('/api/likes', {
+    //     sessionUser: caller,
+    //     likedUser: callee
+    //   });
+    // }
   },
 
   mounted: function(){
     console.log("GETTING YOUR MATCHES")
-    this.likeCallee()
+    // this.likeCallee()
     this.getMatches()
   }
 };
--- App/Server/handlers/matchesHandler.js
+++ App/Server/handlers/matchesHandler.js
@@ -4,28 +4,26 @@ const User = require('../../Database/models/userModel.js');
 exports.getMatches = function (req, res) {
   let likedUsersToUpload = [];
   User.findOne({username: req.body.sessionUser})
-      //check for mutual likes
-    .then(function (data) {
-      data.likes.forEach(function (likedUser) {
-        console.log('checking', likedUser)
-        User.findOne({username: likedUser})
-        .then(function (userRecord) {
-          console.log('user record', userRecord.likes)
-          if (userRecord.likes.indexOf(req.body.sessionUser) !== -1) {
-            console.log('new match!')
-            // add likedUser to matches array of sessionUser
-            likedUsersToUpload.push(likedUser);
-            console.log("array updated", likedUsersToUpload)
-            // to handle adding sessionUser to matches array of likedUser,
-            // this function will run across all users
+  .then(function (data) { //check for mutual likes
+    data.likes.forEach(function (likedUser) {
+      console.log('checking', likedUser)
+      User.findOne({username: likedUser})
+      .then(function (userRecord) {
+        console.log('user record', userRecord.likes)
+        if (userRecord.likes.indexOf(req.body.sessionUser) !== -1) {
+          console.log('new match!')
+          // add likedUser to matches array of sessionUser
+          likedUsersToUpload.push(likedUser);
+          console.log("array updated", likedUsersToUpload)
+          if(likedUser === data.likes[data.likes.length-1]){ //if at end of list, send response
+            res.send(likedUsersToUpload)
           }
-        });
-      })
-  })
-
-  setTimeout(function(){
-    res.send(likedUsersToUpload)
-  }, 5000)
+          // to handle adding sessionUser to matches array of likedUser,
+          // this function will run across all users
+        }
+      });
+    });
+  });
 };
 
 exports.updateLikes = function (req, res) {
